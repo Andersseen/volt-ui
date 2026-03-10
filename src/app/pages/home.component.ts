532659d0f856f08a8c38e407527298db7757d3e7
andersseen <andriipap01@gmail.com>
2026-03-10 12:59:16 +0100

    feat: introduce HomeComponent to showcase UI components and refactor the application structure to support routing.

---
+++ src/app/pages/home.component.ts
@@ -0,0 +1,46 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  VoltButton,
+  VoltBadge,
+  VoltInput,
+  VoltTextarea,
+  VoltLabel,
+  VoltCard,
+  VoltCardHeader,
+  VoltCardTitle,
+  VoltCardDescription,
+  VoltCardContent,
+  VoltCardFooter,
+  VoltSeparator,
+  VoltCheckbox,
+  VoltSwitch,
+  VoltAvatar,
+  VoltAvatarImage,
+  VoltAvatarFallback,
+} from 'volt';
+
+@Component({
+  selector: 'app-home',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    VoltButton,
+    VoltBadge,
+    VoltInput,
+    VoltTextarea,
+    VoltLabel,
+    VoltCard,
+    VoltCardHeader,
+    VoltCardTitle,
+    VoltCardDescription,
+    VoltCardContent,
+    VoltCardFooter,
+    VoltSeparator,
+    VoltCheckbox,
+    VoltSwitch,
+    VoltAvatar,
+    VoltAvatarImage,
+    VoltAvatarFallback,
+  ],
+  templateUrl: './home.component.html',
+})
+export class HomeComponent {}
